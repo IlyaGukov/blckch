9996664374773690859f5ab904ef74ce9e61a138
Ilya <ilyagukov@list.ru>
2017-12-23 18:18:37 +0000

    updated miner

--- miner.py
+++ miner.py
@@ -3,15 +3,23 @@ class Miner:
         self.blockchain = []
         self.transactions = set([])
 		self.block = []
-	def verify_tnx():
-		tnx = Txm_pool.pop()
+	def verify_tnx(self,tnx):
 		if tnx['value'] > 0:
 			if tnx['event'].not_past == True:
 				if tnx['signature']# is valid...
-				self.block.append(tnx)
-			
-    def add_trans(self, transaction):
-        self.transactions.add(transaction)
+					return True
+				else: return False
+			else: return False
+		else: return False
+		
+	def add_tnx(self):
+		tnx = Txm_pool.pop()
+		if verify_tnx() == True:
+			self.block.append(tnx)
+	
+    def verify_block(self, block):
+        for tnx in block:
+		
     def mine(self):
 		self.blockchain.append(self.block)
 		self.block = []
