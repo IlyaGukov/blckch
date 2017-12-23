d548c27a3d07a101ca93f6899f9405210cc862c7
Ilya <ilyagukov@list.ru>
2017-12-23 18:10:59 +0000

    miner minor update

---
+++ miner.py
@@ -0,0 +1,17 @@
+class Miner:
+    def __init__(self):
+        self.blockchain = []
+        self.transactions = set([])
+		self.block = []
+	def verify_tnx():
+		tnx = Txm_pool.pop()
+		if tnx['value'] > 0:
+			if tnx['event'].not_past == True:
+				if tnx['signature']# is valid...
+				self.block.append(tnx)
+			
+    def add_trans(self, transaction):
+        self.transactions.add(transaction)
+    def mine(self):
+		self.blockchain.append(self.block)
+		self.block = []
